f886eee3f5ff9a10fae1a2b39f32f32b807b0b8e
mattl588 <71798926+mattl588@users.noreply.github.com>
2020-09-23 20:30:46 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C_sharp_OOP_3
+{
+    class Program
+    {
+        static void Main(string[] args) //known as a method. 
+        {
+            Console.WriteLine("Area of Rectangle : {0}", ShapeMath.GetArea ("rectangle", 5, 6));
+        }
+    }
+    public static class ShapeMath //static class. Inside must be constant or static method (function)
+    {
+        public static double GetArea (string shape = "", double length1 = 0, double length2 = 0) //takes str, number, number.
+        {
+            if (string.Equals("Rectangle", shape, StringComparison.OrdinalIgnoreCase))
+            {
+                return length1 * length2;
+            }
+            else if (String.Equals("Triangle", shape, StringComparison.OrdinalIgnoreCase)) //some sort of library item allowing strings to be compared. String.Equals. 
+            {
+                return length1 * (length2 / 2);
+            }
+            else if (String.Equals("Circle", shape, StringComparison.OrdinalIgnoreCase))
+            {
+                return 3.14159 * Math.Pow(length1, 2);
+            }
+            else
+            {
+                return -1;
+            }
+            }
+        }
+    }
